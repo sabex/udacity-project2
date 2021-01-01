ba7da61bb665399b1636465340cc1e51aaaa25e7
Raymond Bleach <rableach@yahoo.com>
2021-01-01 16:43:13 -0700

    all services via eureka

--- vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/client/prices/PriceClient.java
@@ -1,3 +1,4 @@
+/* (C)2021 */
 package com.udacity.vehicles.client.prices;
 
 import org.slf4j.Logger;
@@ -6,52 +7,54 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-/**
- * Implements a class to interface with the Pricing Client for price data.
- */
+/** Implements a class to interface with the Pricing Client for price data. */
 @Component
 public class PriceClient {
 
-    private static final Logger log = LoggerFactory.getLogger(PriceClient.class);
+  private static final Logger log = LoggerFactory.getLogger(PriceClient.class);
 
-    private final WebClient client;
+  private final WebClient client;
 
-    @Value("${pricing.endpoint}")
-    public String endpoint;
+  @Value("${pricing.endpoint}")
+  public String endpoint;
 
-    public PriceClient(WebClient pricing) {
-        this.client = pricing;
+  public PriceClient(WebClient pricing) {
+    this.client = pricing;
+  }
+
+  // In a real-world application we'll want to add some resilience
+  // to this method with retries/CB/failover capabilities
+  // We may also want to cache the results so we don't need to
+  // do a request every time
+  /**
+   * Gets a vehicle price from the pricing client, given vehicle ID.
+   *
+   * @param vehicleId ID number of the vehicle for which to get the price
+   * @return Currency and price of the requested vehicle, error message that the vehicle ID is
+   *     invalid, or note that the service is down.
+   */
+  public String getPrice(Long vehicleId) {
+    log.warn(endpoint);
+    try {
+      Price price =
+          client
+              .get()
+              .uri(
+                  uriBuilder ->
+                      uriBuilder
+                          .host(endpoint)
+                          .path("/services/price/")
+                          .queryParam("vehicleId", vehicleId)
+                          .build())
+              .retrieve()
+              .bodyToMono(Price.class)
+              .block();
+
+      return String.format("%s %s", price.getCurrency(), price.getPrice());
+
+    } catch (Exception e) {
+      log.error("Unexpected error retrieving price for vehicle {}", vehicleId, e);
     }
-
-    // In a real-world application we'll want to add some resilience
-    // to this method with retries/CB/failover capabilities
-    // We may also want to cache the results so we don't need to
-    // do a request every time
-    /**
-     * Gets a vehicle price from the pricing client, given vehicle ID.
-     * @param vehicleId ID number of the vehicle for which to get the price
-     * @return Currency and price of the requested vehicle,
-     *   error message that the vehicle ID is invalid, or note that the
-     *   service is down.
-     */
-    public String getPrice(Long vehicleId) {
-        log.warn(endpoint);
-        try {
-            Price price = client
-                    .get()
-//                    .uri(uriBuilder -> uriBuilder
-//                            .path(endpoint + "/services/price/")
-//                            .queryParam("vehicleId", vehicleId)
-//                            .build()
-//                    )
-                    .uri(endpoint + "/services/price?vehicleId=" + vehicleId)
-                    .retrieve().bodyToMono(Price.class).block();
-
-            return String.format("%s %s", price.getCurrency(), price.getPrice());
-
-        } catch (Exception e) {
-            log.error("Unexpected error retrieving price for vehicle {}", vehicleId, e);
-        }
-        return "(consult price)";
-    }
+    return "(consult price)";
+  }
 }
