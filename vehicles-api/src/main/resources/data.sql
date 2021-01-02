aafb1c433dc622859547a1519090d6c69322597e
Raymond Bleach <rableach@yahoo.com>
2021-01-02 15:01:33 -0700

    Update data.sql
    prevent unique id clash on post

--- vehicles-api/src/main/resources/data.sql
+++ vehicles-api/src/main/resources/data.sql
@@ -3,9 +3,9 @@ insert into manufacturer (code, name) values (2, 'an-other');
 
 
 insert into car (id, condition, created_at, body,engine, external_color, fuel_type, mileage, model, model_year, number_of_doors, production_year, lat, lon, manufacturer_code)
-values(1, 'NEW', CURRENT_TIMESTAMP(), 'coupe', 'battery', 'red', 'gas', 153, 'x', 1998, 4, 1998, 20.0, 30.0, 1);
+values(10000, 'NEW', CURRENT_TIMESTAMP(), 'coupe', 'battery', 'red', 'gas', 153, 'x', 1998, 4, 1998, 20.0, 30.0, 1);
 insert into car (id, condition, created_at, body,engine, external_color, fuel_type, mileage, model, model_year, number_of_doors, production_year, lat, lon, manufacturer_code)
-values(2, 'NEW', CURRENT_TIMESTAMP(), 'coupe', 'battery', 'blue', 'gas', 153, 'xplorer', 2004, 4, 1998, 20.0, 30.0, 2);
+values(20000, 'NEW', CURRENT_TIMESTAMP(), 'coupe', 'battery', 'blue', 'gas', 153, 'xplorer', 2004, 4, 1998, 20.0, 30.0, 2);
 insert into car (id, condition, created_at, body,engine, external_color, fuel_type, mileage, model, model_year, number_of_doors, production_year, lat, lon, manufacturer_code)
-values(3, 'NEW', CURRENT_TIMESTAMP(), 'coupe', 'battery', 'red', 'electric', 153, 'A3', 2013, 4, 1998, 20.0, 30.0, 1);
+values(30000, 'NEW', CURRENT_TIMESTAMP(), 'coupe', 'battery', 'red', 'electric', 153, 'A3', 2013, 4, 1998, 20.0, 30.0, 1);
 
